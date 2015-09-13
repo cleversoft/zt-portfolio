900dde0aeef2a2f7386e741b009991650c01a2d0
Dung Tran <tad88.dev@gmail.com>
2015-09-13 20:14:24 +0700

    + Add error controllers

---
+++ administrator/components/com_ztportfolio/controllers/error.php
@@ -0,0 +1,9 @@
+<?php
+defined('_JEXEC') or die;
+
+class ZtPortfolioControllerError extends JControllerForm
+{
+    function display($cachable = false, $urlparams = array()) {
+        parent::display($cachable, $urlparams);
+    }
+}
