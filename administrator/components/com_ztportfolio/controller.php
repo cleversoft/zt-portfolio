a7c5619c3c4a2f62b3365121187474cd071b3722
Dung Tran <tad88.dev@gmail.com>
2015-09-15 23:09:33 +0700

    - Remove example code

--- administrator/components/com_ztportfolio/controller.php
+++ administrator/components/com_ztportfolio/controller.php
@@ -14,10 +14,4 @@ class ZtPortfolioController extends JControllerLegacy
         parent::display($cachable, $urlparams);
     }
 
-
-    public function error(){
-        $view = $this->getView('Error', 'html', 'ZtPortfolioView');
-        $view->display();
-    }
-    
 }
