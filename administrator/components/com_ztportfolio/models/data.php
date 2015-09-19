0afa17d925a65b773726c2ace0a67ba60e857cd3
Dung Tran <tad88.dev@gmail.com>
2015-09-19 23:24:33 +0700

    ^ Update data model

--- administrator/components/com_ztportfolio/models/data.php
+++ administrator/components/com_ztportfolio/models/data.php
@@ -7,6 +7,114 @@ class ZtPortfolioModelData extends JModelLegacy
 {
 
     /**
+     * Portfolio status
+     */
+    const STATUS_DRAFT = 1;
+    const STATUS_PUBLIC = 2;
+    const STATUS_TRASH = 3;
+
+    /**
+     * Create new portfolio
+     * @param type $category
+     * @param type $title
+     * @param type $thumbnail
+     * @param type $content
+     * @param type $status
+     * @return type
+     */
+    public function create($category, $title, $thumbnail, $content, $status = ZtPortfolioModelData::STATUS_DRAFT)
+    {
+        $table = $this->getTable();
+        $table->category = $category;
+        $table->status = $status;
+        $table->title = $title;
+        $table->thumbnail = $thumbnail;
+        $table->content = $content;
+        return $table->store();
+    }
+
+    /**
+     * Get portfolio data by key
+     * @param type $id
+     * @return type
+     */
+    public function load($id)
+    {
+        $table = $this->getTable();
+        $table->load($id);
+        return $table->getProperties();
+    }
+
+    /**
+     * Update portfolio
+     * @param type $id
+     * @param type $category
+     * @param type $title
+     * @param type $thumbnail
+     * @param type $content
+     * @param type $status
+     * @return type
+     */
+    public function update($id, $category, $title, $thumbnail, $content, $status)
+    {
+        $table = $this->getTable($id);
+        $table->category = $category;
+        $table->status = $status;
+        $table->title = $title;
+        $table->thumbnail = $thumbnail;
+        $table->content = $content;
+        return $table->store();
+    }
+
+    /**
+     * List all portfolios
+     */
+    public function listAll()
+    {
+        $db = $this->getDbo();
+        $query = $db->getQuery(true);
+        $query->select('*')
+                ->from($db->quoteName('#__ztportfolio_data'))
+                ->order($db->quoteName('id'));
+        return $db->setQuery($query)
+                        ->loadAssocList();
+    }
+    
+    /**
+     * List all portfolio by status
+     * @param type $status
+     * @return type
+     */
+    public function listAllByStatus($status){
+        $db = $this->getDbo();
+        $query = $db->getQuery(true);
+        $query->select('*')
+                ->from($db->quoteName('#__ztportfolio_data'))
+                ->where($db->quoteName('status') . '=' . $db->quote($status))
+                ->order($db->quoteName('id'));
+        return $db->setQuery($query)
+                        ->loadAssocList();
+    }
+
+    /**
+     * List by category
+     * @param type $category
+     * @return type
+     */
+    public function listByCategory($category)
+    {
+        $db = $this->getDbo();
+        $query = $db->getQuery(true);
+        $query->select('*')
+                ->from($db->quoteName('#__ztportfolio_data'))
+                ->where($db->quoteName('category') . '=' . $db->quote($category))
+                ->order($db->quoteName('id'));
+        echo($query);
+        return $db->setQuery($query)
+                        ->loadAssocList();
+    }
+
+    /**
      * Get data  table
      * @param type $type
      * @param type $prefix
@@ -15,9 +123,7 @@ class ZtPortfolioModelData extends JModelLegacy
      */
     public function getTable($type = 'Data', $prefix = 'ZtPortfolioTable', $config = array())
     {
-        $table = JTable::getInstance($type, $prefix, $config);
-
-        return $table;
+        return JTable::getInstance($type, $prefix, $config);
     }
 
 }
