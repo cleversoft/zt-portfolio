cbaf42dd1a77289417679ba88a6601be07cd5d7a
Dung Tran <tad88.dev@gmail.com>
2015-09-26 16:25:01 +0700

    ^Update toolbar link

--- administrator/components/com_ztportfolio/helpers/toolbar.php
+++ administrator/components/com_ztportfolio/helpers/toolbar.php
@@ -27,9 +27,9 @@ class ZtPortfolioHelperToolbar
             $(\'div#toolbar\').append(html);
         }
         $(w.document).ready(function(){
-                addCustomToolBar(\'' . JRoute::_('index.php?option=com_ztportfolio') . '\', \'home\', \'' . JText::_('COM_PORTFOLIO_HOME') . '\', \'btn btn-small btn-success\');
-                addCustomToolBar(\'' . JRoute::_('index.php?option=com_ztportfolio&view=categories') . '\', \'list-2\', \'' . JText::_('COM_PORTFOLIO_CATEGORIES_MANAGER') . '\', \'btn btn-small btn-small\');
-                addCustomToolBar(\'' . JRoute::_('index.php?option=com_ztportfolio&view=data') . '\', \'paragraph-center\', \'' . JText::_('COM_PORTFOLIO_CONTENT_MANAGER') . '\', \'btn btn-small btn-primary\');
+                addCustomToolBar(\'' . JRoute::_('index.php?option=com_ztportfolio') . '\', \'home\', \'' . JText::_('COM_ZTPORTFOLIO_HOME') . '\', \'btn btn-small btn-success\');
+                addCustomToolBar(\'' . JRoute::_('index.php?option=com_ztportfolio&task=categories.display') . '\', \'list-2\', \'' . JText::_('COM_ZTPORTFOLIO_CATEGORIES_MANAGER') . '\', \'btn btn-small btn-small\');
+                addCustomToolBar(\'' . JRoute::_('index.php?option=com_ztportfolio&task=data.display') . '\', \'paragraph-center\', \'' . JText::_('COM_ZTPORTFOLIO_CONTENT_MANAGER') . '\', \'btn btn-small btn-primary\');
             });
     })(window);
 ');
