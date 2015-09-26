238f70d1d45e7e7fcd859dd1a9e1fd17670c3171
Dung Tran <tad88.dev@gmail.com>
2015-09-26 16:24:28 +0700

    ^Update to zooframework

--- administrator/components/com_ztportfolio/ztportfolio.php
+++ administrator/components/com_ztportfolio/ztportfolio.php
@@ -18,7 +18,13 @@ if (!JFactory::getUser()->authorise('core.manage', 'com_ztautolinks')) {
 
 jimport('joomla.application.component.controller');
 
-$document = JFactory::getDocument();
+ZtFramework::registerExtension(__DIR__ . '/extension.json');
+
+$ztPath = ZtPath::getInstance();    
+$ztAsset = ZtAssets::getInstance();
+
+$ztAsset->addScript('com_ztportfolio://assets/js/admin.script.js');
+$ztAsset->addStyleSheet('com_ztportfolio://assets/css/admin.style.css');
 
 /* Register tables directory */
 JTable::addIncludePath(__DIR__ . '/tables');
@@ -29,5 +35,5 @@ if(JFolder::exists(JPATH_ROOT . '/plugins/system/zooframework') && defined('ZTFR
     $controller->execute(JFactory::getApplication()->input->getCmd('task'));
     $controller->redirect();
 }else{
-    return JError::raiseWarning(404, JText::_('COM_PORTFOLIO_ERROR_ZOOFRAMEWORK_REQUIRED'));
+    return JError::raiseWarning(404, JText::_('COM_ZTPORTFOLIO_ERROR_ZOOFRAMEWORK_REQUIRED'));
 }
