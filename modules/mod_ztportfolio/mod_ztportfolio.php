bdbf075a50e9835ebfedb3045b199b06e8ebda8b
Khang Vu Minh <khangvm530@gmail.com>
2016-02-29 02:21:24 +0700

    update get portfolio

--- modules/mod_ztportfolio/mod_ztportfolio.php
+++ modules/mod_ztportfolio/mod_ztportfolio.php
@@ -8,11 +8,15 @@ require_once __DIR__ . '/helper.php';
 $layout = $params->get('layout', 'default');
 $number = $params->get('number', 10);
 $readmore = $params->get('show_loadmore', 1);
+$orderby = $params->get('orderby', 'ASC');
 
-$portfolios = ModZtPortfolioHelper::getPortfolios($number);
-$tags = ModZtPortfolioHelper::getTags();
+
+$portfolios = ModZtPortfolioHelper::getPortfolios($number = 10, $orderby = 'DESC');
+
+$tags = ModZtPortfolioHelper::getTags($number = null );
 
 $document = JFactory::getDocument();
+
 $document->addScript(JUri::base() . 'modules/mod_ztportfolio/core/assets/js/masonry.pkgd.min.js');
 $document->addScript(JUri::base() . 'modules/mod_ztportfolio/core/assets/js/isotope.min.js');
 $document->addStyleSheet(JUri::base() . 'modules/mod_ztportfolio/core/assets/css/portfolio.css');
