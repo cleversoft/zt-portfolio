5d2070890dd3bcde460695dc0dcb4d69316c0313
khangvm53 <khangvm530@gmail.com>
2016-03-17 17:14:53 +0700

    update isotope

--- components/com_ztportfolio/assets/js/ztportfolio.js
+++ components/com_ztportfolio/assets/js/ztportfolio.js
@@ -7,36 +7,7 @@
 
 jQuery(function($) {
 
-	var $container 	= $('.zt-portfolio-items');
-	
-	$(window).load(function() {
-		var $sizer = $container.find('.shuffle__sizer');
 
-		$container.shuffle({
-			itemSelector: '.zt-portfolio-item',
-			sequentialFadeDelay: 150,
-			sizer: $sizer
-		});
-	});
-
-
-	// Filters
-	$('.zt-portfolio-filter li a').on('click', function(event){
-		event.preventDefault();
-		var $self = $(this);
-		var $this = $(this).parent();
-		
-		if($this.hasClass('active')) {
-			return;
-		}
-
-		$self.closest('ul').children().removeClass('active');
-		$self.parent().addClass('active');
-
-		$container.shuffle( 'shuffle', $this.data('group') );
-	});
-
-	
 
 });
 
